a6bd1a43ec697f96338eb915c99498c566a7af52
Abbe <abdennourbesselma19@gmail.com>
2026-06-08 14:58:56 +0200

    Update project files

--- src/pages/AdminItems.jsx
+++ src/pages/AdminItems.jsx
@@ -189,9 +189,8 @@ export default function AdminItems() {
           onClick={e => e.stopPropagation()}
         >
           <a href={`/items/${openMenu}`} className="dot-menu__item">
-           <a href={`/items/${item.id}`} className="admin-btn admin-btn--small admin-btn--edit"></a>
+            Modify
           </a>
-         
           <button
             onClick={() => { setShowDeleteConfirm(openMenu); setOpenMenu(null); }}
             className="dot-menu__item dot-menu__item--delete"
